91cbd2e62008fe4eeb0b405e019744464f0bc376
Haroon <itx.haroon1@gmail.com>
2024-11-01 03:00:12 +0500

    platform icons added to cards

---
+++ game-hub/src/components/PlatformIcons.tsx
@@ -0,0 +1,31 @@
+import { Platform } from "../hooks/useGames"
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { IconType } from "react-icons";
+import { HStack, Icon } from "@chakra-ui/react";
+
+interface PlatformIconsProps {
+    platforms: Platform[];
+}
+
+export const PlatformIcons = ({ platforms }: PlatformIconsProps) => {
+    const iconsMap : { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+
+  return (
+    <HStack marginY={1}>
+    {platforms.map((platform) => <Icon as={iconsMap[platform.slug]} color='gray.500' />)}
+    </HStack>
+  )
+}
